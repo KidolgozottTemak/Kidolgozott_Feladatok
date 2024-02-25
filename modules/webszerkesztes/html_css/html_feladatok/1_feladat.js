e6fd981c7cb871b5a8a3c29331a064594b66addd
pepe1973 <bodrogipr@gmail.com>
2024-02-25 20:18:57 +0100

    2024.02.25.

--- modules/webszerkesztes/html_css/html_feladatok/1_feladat.js
+++ modules/webszerkesztes/html_css/html_feladatok/1_feladat.js
@@ -1,27 +1,103 @@
 let aktiv = '1. Feladat';
+let i = 1;
 
 let adatok = [
     {
         anyag: [
-            ['h1', 'Bevezetés'],
-            ['h2', 'A tanulási terület tartalmi összefoglalója'],
-            ['img', '../programozasi_alapok/images/kep_001.jpg'],
+            ['h1', '1. Feladat'],
+            ['img', '../images/html_feladat.png'],
             [
                 'p',
-                'A kódolás és a programozás képessége ma már nemcsak a szoftverfejlesztők számára nélkülözhetetlen, az összes egyéb területen dolgozó informatikus és távközlési szakember számára elengedhetetlen. Ezért fontos, hogy a közös alapozás jegyében minden tanuló megszeresse a kódolást, fejlett problémamegoldó és algoritmizáló képességre tegyen szert, és egyszerűbb problémák kezelését végző alkalmazásokat tudjon készíteni. Elengedhetetlen, hogy minden tanuló képes legyen webes környezetben dolgozni. A tanulási terület oktatásának fontos feladata az is, hogy a tanulókat megtanítsa csapatban dolgozni, valamint képessé tegye a közös munkát segítő forráskódkezelők és a csoportmunkát támogató online eszközök kezelésére.',
+                'Hozzunk létre egy index.html állományt. A head elemben soroljunk fel minél több meta elemet és írjuk le, hogy mi a feladatuk.',
             ],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['img', '../images/kep_013.png', 'kod'],
+            ['p', 'A title elem felel a weboldal címéért.'],
             [
                 'p',
-                'A tantárgy a közös ágazati alapozás részeként olyan programozási és kódolási alapkészségeket ad, amelyek minden informatika és távközlési ágazatban tanuló fiatal számára szükségesek',
+                'Ez fog megjelenni az index.html-hez tartozó weboldal fülén és a könyvjelzők között is.',
             ],
+            ['p', 'Hasznos lehet a SEO folyamán is.'],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['img', '../images/kep_014.png', 'kod'],
+            ['p', 'Különböző meta elemek.'],
+            ['p', 'Hasznosak lehetnek a SEO folyamán is.'],
             [
-                'p',
-                'A témakör elsődleges célja a tanulói érdeklődés felkeltése, a motiváció erősítése a programozás tantárgy tanulására. A tanulók megismerkednek az egyéni tanulás és önfejlesztés lehetőségeivel is',
+                'ul',
+                [
+                    'charset: a karakterkódolásért felel',
+                    'viewport: a reszponzivitáshoz kell',
+                    'author: az oldal szerzője',
+                    'keywords: kulcsszavak az oldal megtalálásához',
+                    'description: az oldal tartalmának a leírása',
+                ],
             ],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['img', '../images/kep_015.png', 'kod'],
+            ['p', 'Különböző meta http-equiv elemek.'],
+            ['p', 'Hasznosak lehetnek a SEO folyamán is.'],
             [
-                'a',
-                'https://api.ikk.hu/storage/uploads/files/ptt_informatika_szoftverfejleszto_es_-tesztelo_2020pdf-1599123461507.pdf',
-                'PTT - Programtanterv',
+                'ul',
+                [
+                    'refresh: az oldal frissítése 5 másodpercenként',
+                    'refresh: az oldal azonnali frissítése a url-ben megadott oldalra',
+                ],
+            ],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['img', '../images/kep_016.png', 'kod'],
+            ['p', 'Különböző link elemek.'],
+            ['p', 'Külső erőforrások (stíluslapok, képek stb.) beimportálása.'],
+            [
+                'ul',
+                [
+                    'stylesheet: külső stíluslap beimportálása',
+                    'shortcut icon: favicon beimportálása (kis kép a fülön a cím előtt)',
+                ],
+            ],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['img', '../images/kep_017.png', 'kod'],
+            ['p', 'Különböző script elemek.'],
+            ['p', 'Külső erőforrások (szriptek) beimportálása.'],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['img', '../images/kep_018.png', 'kod'],
+            ['p', 'Belső stíluslap elemek.'],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['img', '../images/kep_019.png', 'kod'],
+            ['p', 'A base elemet két dologra használjuk:'],
+            [
+                'ul',
+                [
+                    'Az oldalon szereplő hivatkozásokhoz és képekhez egy közös mappa létrehozása (tartalom), és az erre való hivatkozás.',
+                    'A hivatkozások új oldalon nyíljanak meg.',
+                ],
             ],
         ],
     },
--- modules/webszerkesztes/html_css/html_feladatok/index.js
+++ modules/webszerkesztes/html_css/html_feladatok/index.js
@@ -1,16 +1,112 @@
 let aktiv = 'Visual Studio Code telepítése és beállítása';
+let i = 1;
 
 let adatok = [
     {
         anyag: [
             ['h1', 'Bevezetés'],
-            ['h2', 'A tanulási terület tartalmi összefoglalója'],
+            ['h2', 'Ismerkedés a Visual Studio Code kódszerkesztővel'],
             ['img', '../../html_css/images/kep_001.png'],
             [
                 'p',
                 'A Visual Studio Code ingyenes, nyílt forráskódú kódszerkesztő, melyet a Microsoft fejleszt Windows, Linux és macOS operációs rendszerekhez. Támogatja a hibakeresőket, valamint beépített Git támogatással rendelkezik, továbbá képes az intelligens kódkiegészítésre az IntelliSense segítségével.',
             ],
             ['a', 'https://code.visualstudio.com/', 'Visual Studio Code'],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['h2', 'A Visual Studio Code letöltése'],
+            ['img', '../../html_css/images/kep_002.png'],
+            [
+                'p',
+                'Látogassunk el a szerkesztő honlapjára, és töltsük le a nekünk megfelelőt.',
+            ],
+            ['a', 'https://code.visualstudio.com/', 'Visual Studio Code'],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['h2', 'A Visual Studio Code telepítése'],
+            ['p', 'Fogadjuk el a szabványos szerződés feltételeit.'],
+            ['img', '../../html_css/images/kep_003.png'],
+            ['p', 'Jelöljük be az összes négyzetet.'],
+            ['img', '../../html_css/images/kep_004.png'],
+            ['p', 'Indítsuk el a telepítőt.'],
+            ['img', '../../html_css/images/kep_005.png'],
+            ['p', 'Állítsuk be a szerkesztő kinézetét az igényeink szerint.'],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['h2', 'A Visual Studio Code beállítása'],
+            [
+                'p',
+                'Az Extensions fülön állítsuk be a következő kiterjesztéseket.',
+            ],
+            ['img', '../../html_css/images/kep_006.png'],
+            ['p', 'Kiterjesztések.'],
+            ['img', '../../html_css/images/kep_007.png'],
+            ['p', 'Egyéb fontos kiterjesztések.'],
+            ['img', '../../html_css/images/kep_008.png'],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['h2', 'A Prettier kiterjesztés beállítása'],
+            [
+                'p',
+                'Nyissuk meg a Settings fület miután a Manage fülre kattintottunk a szerkesztő felületének bal alsó sarkában.',
+            ],
+            ['img', '../../html_css/images/kep_009.png'],
+            [
+                'p',
+                'Írjuk be a keresőbe a formatter kifejezést, majd a legördülő listából válasszuk ki a Prettier-t.',
+            ],
+            ['img', '../../html_css/images/kep_010.png'],
+            [
+                'p',
+                'Majd, ha kell, írjuk be a keresőbe a format kifejezést és jelöljük be a következő két opciót.',
+            ],
+            ['img', '../../html_css/images/kep_011.png'],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['h2', 'Mappastruktúra beállítása'],
+            [
+                'p',
+                'Nyissuk meg a Settings fület miután a Manage fülre kattintottunk a szerkesztő felületének bal alsó sarkában.',
+            ],
+            ['img', '../../html_css/images/kep_009.png'],
+            [
+                'p',
+                'Írjuk be a keresőbe az explorer.compact folder kifejezést, majd a távolítsuk el a kijelölést. Ennek hatására a mappastruktúrában a beágyazott mappák egymás alatt és nem egy perjellel elválasztva egymás mellett fognak megjelenni.',
+            ],
+            ['img', '../../html_css/images/kep_012.png'],
+        ],
+    },
+    {
+        anyag: [
+            ['h1', `${i++}. Lépés`],
+            ['h2', 'Emmet'],
+            ['img', '../../html_css/images/kep_013.jpg'],
+            [
+                'p',
+                'Az Emmet olyan beépülő modulok készlete szövegszerkesztőkhöz, amelyek nagy sebességű kódolást és szerkesztést tesznek lehetővé HTML, XML, XSLT és más strukturált kódformátumokban a tartalomsegítő segítségével.',
+            ],
+            ['a', 'https://emmet.io/', 'Emmet'],
+            [
+                'a',
+                'https://code.visualstudio.com/docs/editor/emmet',
+                'Emmet a Visual Studio Code-ban',
+            ],
+            ['a', 'https://docs.emmet.io/cheat-sheet/', 'Emmet Cheat Sheet'],
         ],
     },
 ];
